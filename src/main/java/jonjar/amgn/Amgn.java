1215111d59566224e9eb8387d77832effc4d151a
macahmy <macham2001@gmail.com>
2021-01-10 02:03:54 +0900

    미묘한수정

--- src/main/java/jonjar/amgn/Amgn.java
+++ src/main/java/jonjar/amgn/Amgn.java
@@ -3,8 +3,13 @@ package jonjar.amgn;
 import net.fabricmc.api.ModInitializer;
 
 public class Amgn implements ModInitializer {
+
+    public static final String MODID ="amgn";
+
     @Override
     public void onInitialize() {
+
+        System.out.println(MODID+" Intialized");
 
     }
 }
