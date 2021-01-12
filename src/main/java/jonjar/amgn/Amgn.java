b1e1c88571ee0060bee725b34f5ea699fd69c6ad
khy010802 <khy010802@naver.com>
2021-01-12 19:50:36 +0900

    이건무슨

--- src/main/java/jonjar/amgn/Amgn.java
+++ src/main/java/jonjar/amgn/Amgn.java
@@ -5,10 +5,18 @@ import jonjar.amgn.registry.ModFluids;
 import jonjar.amgn.registry.ModItems;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.fabricmc.fabric.impl.registry.sync.FabricRegistry;
+import net.minecraft.block.Block;
+import net.minecraft.client.util.ModelIdentifier;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+import java.lang.reflect.Field;
+import java.util.Iterator;
 
 public class Amgn implements ModInitializer {
 
@@ -21,6 +29,22 @@ public class Amgn implements ModInitializer {
 
     @Override
     public void onInitialize() {
+
+        for(Block b : Registry.BLOCK){
+            if(b.getTranslationKey().contains("minecraft")){
+                try{
+                    Class c = Class.forName("net.minecraft.block.AbstractBlock");
+                    Field field = c.getDeclaredField("slipperiness");
+                    field.setAccessible(true);
+                    field.set(b, 1.11F);
+                    field.setAccessible(false);
+                } catch(Exception e){
+                    e.printStackTrace();
+                }
+
+            }
+
+        }
 
 
         ModFluids.registryFluids();//액체 등록
