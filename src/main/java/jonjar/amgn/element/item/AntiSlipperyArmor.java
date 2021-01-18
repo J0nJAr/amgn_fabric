a076209af96fa8917d134baf9ee5de989e6541ba
macahmy <macham2001@gmail.com>
2021-01-18 12:31:52 +0900

    스파이크 머터리얼

--- src/main/java/jonjar/amgn/element/item/AntiSlipperyArmor.java
+++ src/main/java/jonjar/amgn/element/item/AntiSlipperyArmor.java
@@ -1,11 +1,61 @@
 package jonjar.amgn.element.item;
 
+import jonjar.amgn.registry.ModItems;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.item.ArmorItem;
 import net.minecraft.item.ArmorMaterial;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
 
 public class AntiSlipperyArmor extends ArmorItem {
+    private static final int[] BASE_DURABILITY = new int[] {14, 15, 16, 11};
+    private static final int[] PROTECTION_VALUES = new int[] {4, 4, 5, 2};
+
     public AntiSlipperyArmor(ArmorMaterial material, EquipmentSlot slot, Settings settings) {
         super(material, slot, settings);
+    }
+    public static class CustomArmorMaterial implements ArmorMaterial {
+        int X = 3;
+
+        @Override
+        public int getDurability(EquipmentSlot slot) {
+            return BASE_DURABILITY[slot.getEntitySlotId()] * X;
+        }
+
+        @Override
+        public int getProtectionAmount(EquipmentSlot slot) {
+            return PROTECTION_VALUES[slot.getEntitySlotId()];
+        }
+
+        @Override
+        public int getEnchantability() {
+            return X;
+        }
+
+        @Override
+        public SoundEvent getEquipSound() {
+            return SoundEvents.ITEM_ARMOR_EQUIP_CHAIN;
+        }
+
+        @Override
+        public Ingredient getRepairIngredient() {
+            return Ingredient.ofItems(ModItems.ICE_BOOTS);
+        }
+
+        @Override
+        public String getName() {
+            return "spike";
+        }
+
+        @Override
+        public float getToughness() {
+            return 0.0F;
+        }
+
+        @Override
+        public float getKnockbackResistance() {
+            return 0.0F;
+        }
     }
 }
