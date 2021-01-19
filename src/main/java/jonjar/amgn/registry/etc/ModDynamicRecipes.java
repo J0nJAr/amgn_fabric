614c6277df58380c1dec2eaa7dca9d3590d51154
machamy <macham2001@gmail.com>
2021-01-19 13:14:20 +0900

    임시커밋, 기계만드는중

--- src/main/java/jonjar/amgn/registry/etc/ModDynamicRecipes.java
+++ src/main/java/jonjar/amgn/registry/etc/ModDynamicRecipes.java
@@ -2,11 +2,6 @@ package jonjar.amgn.registry.etc;
 
 import com.google.gson.JsonObject;
 import jonjar.amgn.registry.BaseRegistry;
-import net.minecraft.block.CraftingTableBlock;
-import net.minecraft.data.server.recipe.ShapelessRecipeJsonFactory;
-import net.minecraft.item.Item;
-import net.minecraft.recipe.*;
-import net.minecraft.tag.ItemTags;
 
 import java.util.ArrayList;
 
@@ -16,10 +11,6 @@ public class ModDynamicRecipes extends BaseRegistry {
 
 
     public void register(){
-        for(Item flower: ItemTags.FLOWERS.values()){
-            RecipeFinder rf = new RecipeFinder();
-            if(rf.countRecipeCrafts())
 
-        }
     }
 }
